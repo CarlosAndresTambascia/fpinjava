6d5ff9caf56d50131b2704682ce13eabcdc725e9
ctambascia <v-ctambascia@expedia.com>
2020-07-03 14:58:11 -0300

    EXERCISE 2.5

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_05/Function.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_05/Function.java
@@ -5,6 +5,6 @@ public interface Function<T, U> {
   U apply(T arg);
 
   static <T, U, V> Function<Function<U, V>, Function<Function<T, U>, Function<T, V>>> higherCompose() {
-    throw new RuntimeException("To be implemented.");
+    return firstFnArg -> secondFnArg -> returnFunctionArg -> firstFnArg.apply(secondFnArg.apply(returnFunctionArg));
   }
 }
