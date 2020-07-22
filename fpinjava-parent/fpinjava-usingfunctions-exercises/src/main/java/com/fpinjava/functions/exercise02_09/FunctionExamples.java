7e4211e8522e953d53c845f19cb7a9fb53638c3d
ctambascia <v-ctambascia@expedia.com>
2020-07-22 10:18:57 -0300

    EXERCISE 2.9

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_09/FunctionExamples.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_09/FunctionExamples.java
@@ -5,6 +5,6 @@ public class FunctionExamples {
   private static String format = "%s, %s, %s, %s";
 
   public static <A, B, C, D> Function<A, Function<B, Function<C, Function<D, String>>>> f() {
-    throw new RuntimeException("To be implemented.");
+    return f1 -> f2 -> f3 -> f4 -> String.format(format, f1, f2, f3, f4);
   }
 }
