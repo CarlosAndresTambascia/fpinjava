33057d41015c6cde333ff6d6a7882d62bc20cc4e
ctambascia <v-ctambascia@expedia.com>
2020-07-22 10:31:40 -0300

    EXERCISE 2.10

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_10/FunctionExamples.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_10/FunctionExamples.java
@@ -3,6 +3,6 @@ package com.fpinjava.functions.exercise02_10;
 public class FunctionExamples {
 
   public static <A, B, C> Function<A, Function<B, C>> curry(Function<Tuple<A, B>, C> f) {
-    throw new RuntimeException("To be implemented.");
+    return f1 -> f2 -> f.apply(new Tuple<>(f1, f2));
   }
 }
