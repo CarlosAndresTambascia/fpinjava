f78d2b07878c8c83c4f7f90310aecff74960bf8f
ctambascia <v-ctambascia@expedia.com>
2020-09-01 12:41:56 -0300

    Exercise 3.11.

--- fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_11/Range.java
+++ fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_11/Range.java
@@ -1,11 +1,21 @@
 package com.fpinjava.makingjavafunctional.exercise03_11;
 
+import static com.fpinjava.makingjavafunctional.exercise03_10.CollectionUtilities.list;
+
+import com.fpinjava.makingjavafunctional.exercise03_10.CollectionUtilities;
+
 import java.util.List;
 
 
 public class Range {
 
   public static List<Integer> range(int start, int end) {
-    throw new RuntimeException("To be implemented");
+    List<Integer> result = list();
+    int temp = start;
+    while(temp < end){
+      result = CollectionUtilities.append(result, temp);
+      temp = temp +1;
+    }
+    return result;
   }
 }
